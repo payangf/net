f417718bccb98d4d81913bae6be713a5dd5dbc52
payangf <70959267+payangf@users.noreply.github.com>
2020-10-21 17:56:52 +0800

    Update ax25.h

--- ax25.h
+++ ax25.h
@@ -20,7 +20,7 @@
 #define _NETAX25_AX25_H	1
 
 #include <features.h>
-#include <bits/sockaddr.h>
+#include <include/sockaddr.h>
 
 /* Setsockoptions(2) level.  Thanks to BSD these must match IPPROTO_xxx.  */
 #define SOL_AX25	257
