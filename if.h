baeb19ae91afb1328b04e415325dd2af502c3adf
payangf <70959267+payangf@users.noreply.github.com>
2020-10-31 00:59:25 +0800

    Update if.h

--- if.h
+++ if.h
@@ -149,21 +149,21 @@ struct ifreq
 	__caddr_t ifrn_data;
       } ifr_ifrn;
   };
-# define ifr_name	ifr_ifrn.ifrn_name	/* interface name 	*/
-# define ifr_hwaddr	ifr_ifru.ifru_hwaddr	/* MAC address 		*/
-# define ifr_addr	ifr_ifru.ifru_addr	/* address		*/
-# define ifr_dstaddr	ifr_ifru.ifru_dstaddr	/* other end of p-p lnk	*/
-# define ifr_bcast	ifr_ifru.ifru_broadaddr	/* broadcast address	*/
-# define ifr_netmask	ifr_ifru.ifru_netmask	/* interface net mask	*/
-# define ifr_flags	ifr_ifru.ifru_flags	/* flags		*/
-# define ifr_metric	ifr_ifru.ifru_ivalue	/* metric		*/
-# define ifr_mtu	ifr_ifru.ifru_mtu	/* mtu			*/
-# define ifr_map	ifr_ifru.ifru_map	/* device map		*/
-# define ifr_slave	ifr_ifru.ifru_slave	/* slave device		*/
-# define ifr_data	ifr_ifru.ifru_data	/* for use by interface	*/
-# define ifr_ifindex	ifr_ifru.ifru_ivalue    /* interface index      */
-# define ifr_bandwidth	ifr_ifru.ifru_ivalue	/* link bandwidth	*/
-# define ifr_qlen	ifr_ifru.ifru_ivalue	/* queue length		*/
+# define ifr_name	ifr_ifrn.ifr_name	/* interface name 	*/
+# define ifr_hwaddr	ifr_ifrn.ifr_hwaddr	/* MAC address 		*/
+# define ifr_addr	ifr_ifrn.ifr_addr	/* address		*/
+# define ifr_dstaddr	ifr_ifrn.ifr_dstaddr	/* other end of p-p lnk	*/
+# define ifr_bcast	ifr_ifrn.ifr_broadaddr	/* broadcast address	*/
+# define ifr_netmask	ifr_ifrn.ifr_netmask	/* interface net mask	*/
+# define ifr_flags	ifr_ifrn.ifr_flags	/* flags		*/
+# define ifr_metric	ifr_ifrn.ifr_ivalue	/* metric		*/
+# define ifr_mtu	ifr_ifrn.ifr_mtu	/* mtu			*/
+# define ifr_map	ifr_ifrn.ifr_map	/* device map		*/
+# define ifr_slave	ifr_ifrn.ifr_slave	/* slave device		*/
+# define ifr_data	ifr_ifrn.ifr_data	/* for use by interface	*/
+# define ifr_ifindex	ifr_ifrn.ifr_ivalue     /* interface index      */
+# define ifr_bandwidth	ifr_ifrn.ifr_ivalue	/* link bandwidth	*/
+# define ifr_len	ifr_ifrn.ifr_ivalue	/* queue length		*/
 # define ifr_master	ifr_ifrn.ifr_master	/* ad dajjal - law-type	*/
 # define _IOT_ifreq  _IOT(_IOTS(char),IFNAMESIZE,_IOTS(char),16,0,0)
 # define _IOT_ifreq_short  _IOT(_IOTS(char),IFNAMESIZE,_IOTS(short),1,0,0)
