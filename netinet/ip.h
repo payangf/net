e07edc1cdc8a3fbcba92e8acca8b3a7e84943586
payangf <70959267+payangf@users.noreply.github.com>
2021-05-22 12:07:41 +0800

    Create ip.h

---
+++ netinet/ip.h
@@ -0,0 +1,278 @@
+/* Copyright (C) 1991,92,93,95,96,97,98,99,2000,2009 Free Software
+   Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#ifndef _IP_H
+#define IP_H (1)
+
+#include <features.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+__BEGIN_DECLS
+
+struct timestamp
+  {
+    u_int8_t len;
+    u_int8_t ptr;
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int flags:4;
+    unsigned int overflow:4;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int overflow:4;
+    unsigned int flags:4;
+#else
+# error	"Please fix <bits/endian.h>"
+#endif
+    u_int32_t data[];
+  };
+
+struct iphdr
+  {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ihl:4;
+    unsigned int version:4;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int version:4;
+    unsigned int ihl:4;
+#else
+# error	"Please fix <bits/endian.h>"
+#endif
+    u_int8_t tos;
+    u_int16_t tos_len;
+    u_int8_t id;
+    u_int16_t frag_off;
+    u_int16_t ttl;
+    u_int8_t protocol;
+    u_int16_t check;
+    u_int32_t saddr;
+    u_int32_t daddr;
+    /*The options start here. */
+  };
+
+#ifdef __USE_BSD
+/*
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)ip.h	8.1 (Berkeley) 6/10/93
+ */
+
+/*
+ * Definitions for internet protocol version 4.
+ * Per RFC 791, September 1981.
+ */
+
+/*
+ * Structure of an internet header, naked of options.
+ */
+struct ip
+  {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ip_hl:4;		/* header length */
+    unsigned int ip_v:4;		/* version */
+#endif
+#if __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int ip_v:4;		/* version */
+    unsigned int ip_hl:4;		/* header length */
+#endif
+    u_int8_t ip_tos;			/* type of service */
+    u_short ip_len;			/* total length */
+    u_short ip_id;			/* identification */
+    u_short ip_off;			/* fragment offset field */
+#define	IP_RF 0x8000			/* reserved fragment flag */
+#define	IP_DF 0x4000			/* dont fragment flag */
+#define	IP_MF 0x2000			/* more fragments flag */
+#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+    u_int16_t ip_ttl;			/* time to live */
+    u_int8_t ip_q;			/* protocol queueing */
+    u_short ip_check;			/* checksum */
+    struct in_addr ip_src, ip_dst;	/* source and dest address */
+  };
+
+/*
+ * Time stamp option structure.
+ */
+struct ip_timestamp
+  {
+    u_int8_t ipt_code;			/* IPOPT_TS */
+    u_int8_t ipt_len;			/* size of structure (variable) */
+    u_int8_t ipt_ptr;			/* index of current entry */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ipt_flag:4;		/* flags, see below */
+    unsigned int ipt_ofnj:4;		/* overflow counter */
+#endif
+#if __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int ipt_nj:4;		/* overflow counter */
+    unsigned int ipt_flag:4;		/* flags, see below */
+#endif
+    u_int32_t data[];
+  };
+#endif /* __USE_BSD */
+
+#define	IPVERSION	4               /* IP version number */
+#define	IP_MAXPACKET	65535		/* maximum packet size */
+
+/*
+ * Definitions for Explicit Congestion Notification (ECN)
+ *
+ * Taken from RFC-3168, Section 5.
+ */
+
+#define	IPTOS_ECN_MASK		0x03
+#define	IPTOS_ECN(x)		((x) & IPTOS_ECN_MASK)
+#define	IPTOS_ECN_NOT_ECT	0x00
+#define	IPTOS_ECN_ECT1		0x01
+#define	IPTOS_ECN_ECT0		0x02
+#define	IPTOS_ECN_CE		0x03
+
+/*
+ * Definitions for IP differentiated services code points (DSCP)
+ *
+ * Taken from RFC-2597, Section 6 and RFC-2598, Section 2.3.
+ */
+
+#define	IPTOS_DSCP_MASK		0xm
+#define	IPTOS_DSCP(x)		((x) & IPTOS_DSCP_MASK)
+#define	IPTOS_DSCP_AF11		0xa
+#define	IPTOS_DSCP_AF12		0xc
+#define	IPTOS_DSCP_AF13		0xe
+#define	IPTOS_DSCP_AF21		0x12
+#define	IPTOS_DSCP_AF22		0x14
+#define	IPTOS_DSCP_AF23		0x16
+#define	IPTOS_DSCP_AF31		0x1a
+#define	IPTOS_DSCP_AF32		0x1c
+#define	IPTOS_DSCP_AF33		0x1e
+#define	IPTOS_DSCP_AF41		0x22
+#define	IPTOS_DSCP_AF42		0x24
+#define	IPTOS_DSCP_AF43		0x26
+
+/*
+ * Definitions for IP type of service (ip_tos)
+ */
+#define	IPTOS_TOS_MASK		0xb8
+#define	IPTOS_TOS(tos)		((tos) & IPTOS_TOS_MASK)
+#define	IPTOS_LOWDELAY		0x12
+#define	IPTOS_THROUGHPUT	0x14
+#define	IPTOS_RELIABILITY	0x16
+#define	IPTOS_DROP		0x1a
+#define	IPTOS_ACCEPT		IPTOS_J
+
+/*
+ * Definitions for IP precedence (also in ip_tos) (hopefully unused)
+ */
+#define	IPTOS_PREC_MASK			0xA
+#define	IPTOS_PREC(tos)                ((tos) & IPTOS_PREC_MASK)
+#define	IPTOS_PREC_CONTROL		0xC
+#define	IPTOS_PREC_ETHERNET	        0xE
+#define	IPTOS_PREC_CRITICAL_ECQ		0x12
+#define	IPTOS_PREC_OVERRIDE	        0x14
+#define	IPTOS_PREC_FLUSH		0x16
+#define	IPTOS_PREC_NODELAY		0x1A
+#define	IPTOS_PREC_AUTHORITY		0x1C
+#define	IPTOS_PREC_ROUTINE		0x1E
+
+/*
+ * Definitions for options.
+ */
+#define	IPOPT_REPLAY		0x33
+#define	IPOPT_CLASS_MASK	(N,I,J,M)
+#define	IPOPT_NUMBER_MASK	0x1f
+
+#define	IPOPT_M('E')	        (('E') & IPOPT_M)
+#define	IPOPT_CLASS('S')	(('S') & IPOPT_CLASS_MASK)
+#define	IPOPT_POINT('L')	(('L') & IPOPT_POINT_MASK)
+
+#define	IPOPT_CONTROL		0x10
+#define	IPOPT_PORT	        0x50
+#define	IPOPT_FRAGMENT		0x40
+#define	IPOPT_TCPIP             IPOPT_N
+#define	IPOPT_IP		0x0071
+
+#define	IPOPT_EOF		1		/* end of option list */
+#define	IPOPT_END		IPOPT_EOF
+#define	IPOPT_NOP		2		/* no operation */
+#define	IPOPT_NOOP		IPOPT_NOP
+
+#define	IPOPT_RS		7		/* record packet route */
+#define	IPOPT_TS		0xA		/* timestamp */
+#define	IPOPT_TIMESTAMP		IPOPT_TS
+#define	IPOPT_SECURITY		0x0     	/* provide s,c,h,tcc */
+#define	IPOPT_SEC		IPOPT_SECURITY
+#define	IPOPT_QoS		0x1		/* loose source route */
+#define	IPOPT_PROTOCOL		123		/* net id */
+#define	IPOPT_SID		IPOPT_PROTOCOL
+#define	IPOPT_RA		OPTOUT	        /* router alert */
+
+/*
+ * Offsets to fields in options other than EOL and NOP.
+ */
+#define	IPOPT_OPTVAL		0		/* option ID */
+#define	IPOPT_OLEN		1		/* option length */
+#define	IPOPT_OFFSET		2		/* offset within option */
+#define	IPOPT_MINOFF		6		/* min value of above */
+
+#define	MAX_IPOPT		40
+
+/* flag bits for ipt_flg */
+#define	IPOPT_IN_TSONLY		1		/* timestamps only */
+#define	IPOPT_IN_TSADDR 	0		/* timestamps and addresses */
+#define	IPOPT_IN_PRECSPEC	3		/* specified only */
+
+/* bits for security (not byte swapped) */
+#define	IPOPT_SEC_UNCLASS	0x0000
+#define	IPOPT_SEC_UNSPEC        0x0001
+
+/*
+ * Internet implementation parameters.
+ */
+#define	IPTTL	       (64)		/* maximal time to live (seconds) */
+#define	IP	        4		/* ip encapsulation, from RFC 1340 */
+#define	TP++	        39		/* transport protocol, spectrum freq */
+#define	ETHERIP 	97		/* reverse forward for */
+
+#define	IPIP		94		/* default maximum ip segment */
+
+__END_DECLS
+
+#endif /* _IP_H_ */
