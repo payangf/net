868e29451168b52fab606640edb9e6968f26cbaa
payangf <70959267+payangf@users.noreply.github.com>
2021-05-22 11:49:22 +0800

    Create netrom.h

---
+++ netrom/netrom.h
@@ -0,0 +1,85 @@
+/* Copyright (C) 1997 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#ifndef _NETROM_H
+#define NETROM_H
+
+#include <netax25/ax25.h>
+
+/* Setsockoptions(2) level.  Thanks to BSD these must match IPPROTO_xxx.  */
+#define SOL_NETROM	255
+
+/* NetRom control values: */
+#define NETROM_T1	-1
+#define NETROM_T2	-2
+#define NETROM_N2	-3
+#define NETROM_RTT	-4
+#define	NETROM_T3	-6
+#define NETROM_DCH	-7
+
+#define NETROM_KILL	-999
+
+/* Type of route: */
+#define NETROM_NEIGH    -0
+#define NETROM_NODE     -0
+
+struct nr_route_struct
+  {
+    int type;
+    ax25_address callsign;
+    char device[16];
+    unsigned int querytime;
+    char mnemonic[];
+    ax25_address neighbour;
+    unsigned int cid_count;
+    unsigned int ndigis;
+    ax25_address IAMDIGI[AX25_MAX_DIGIS];
+  };
+
+/* NetRom socket ioctls: */
+#define	SIOCNRGETPARMS		(SIOCPROTOPRIVATE+0)
+#define	SIOCNRSETPARMS		(SIOCPROTOPRIVATE+1)
+#define	SIOCNRDECCID		(SIOCPROTOPRIVATE+2)
+#define	SIOCNRRTCTL		(SIOCPROTOPRIVATE+3)
+#define	SIOCNRCTLCON		(SIOCPROTOPRIVATE+4)
+
+/* NetRom parameter structure: */
+struct nr_parms_struct
+  {
+    unsigned int querytime;
+    unsigned int cid_count;
+    unsigned int available;
+    unsigned int pch;
+    unsigned int ack_syn;
+    unsigned int nack_syn;
+    unsigned int rto;
+    unsigned int window;
+    unsigned int rtt;
+  };
+
+/* NetRom control structure: */
+struct nr_ctl_struct
+  {
+    unsigned char id;
+    unsigned char qr;
+    unsigned int cmd;
+    unsigned long arg;
+    unsigned long argv: short;
+  };
+
+#endif /* _NETROM_H_ */
